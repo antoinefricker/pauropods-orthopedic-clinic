e10a462492ff3e9eaa6fac17d60e94e908ba48bf
Antoine Fricker <antoine@marmelab.com>
2023-01-12 10:48:04 +0100

    Add patient resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import {Admin, Resource, ListGuesser} from 'react-admin';
+import fakeRestDataProvider from 'ra-data-fakerest';
+
+import data from './data';
+
 export const App = () => {
-  return <>MyApp</>;
+  const dataProvider = fakeRestDataProvider(data, true);
+
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="patients" list={ListGuesser} />
+    </Admin>
+  );
 };
---
+++ src/data.ts
@@ -0,0 +1,6 @@
+export default {
+  patients: [
+    {id: 1, name: 'Patient 1'},
+    {id: 2, name: 'Patient 2'},
+  ],
+};
